211a0f7a8d970f4dc7a42d0683f08655fa49631e
Jonathan Hess <info@movingwater.ch>
2016-07-11 11:34:16 +0200

    new git plugin

--- site/config/config.php
+++ site/config/config.php
@@ -29,27 +29,5 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
 
-c::set('autogit.branch',         'live');
-c::set('autogit.remote.name',    'kirbytest');
-c::set('autogit.remote.branch',  'live'); // same as autogit.branch option
-c::get('autogit.webhook.secret', 'jkergjjiopsbklj58efwef-75rfgw4');
-c::get('autogit.webhook.url',    'autogit');
-c::set('autogit.panel.user',     false);
-c::set('autogit.user.name',      'movingwater');
-c::set('autogit.user.email',     'info@movingwater.ch');
-c::set('autogit.language',       'en');
-c::set('autogit.translation', [
-    'site.update'  => 'Changed site options',
-    'page.create'  => 'Created page %s',
-    'page.update'  => 'Updated page %s',
-    'page.delete'  => 'Deleted page %s',
-    'page.sort'    => 'Sorted page %s',
-    'page.hide'    => 'Hid page %s',
-    'page.move'    => 'Moved page %1$s to %2$s',
-    'file.upload'  => 'Uploaded file %s',
-    'file.replace' => 'Replaced file %s',
-    'file.rename'  => 'Renamed file %s',
-    'file.update'  => 'Updated file %s',
-    'file.sort'    => 'Sorted file %s',
-    'file.delete'  => 'Deleted file %s',
-]);
+c::set('gcapc-branch',         'master');
+c::set('gcapc-commit',         'true');
