7eae9c89682937e5e59c17c23593fcfa81859311
Jonathan Hess <info@movingwater.ch>
2016-07-11 11:07:21 +0200

    inital commit

---
+++ site/config/config.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+
+---------------------------------------
+License Setup
+---------------------------------------
+
+Please add your license key, which you've received
+via email after purchasing Kirby on http://getkirby.com/buy
+
+It is not permitted to run a public website without a
+valid license key. Please read the End User License Agreement
+for more information: http://getkirby.com/license
+
+*/
+
+c::set('license', 'put your license key here');
+
+/*
+
+---------------------------------------
+Kirby Configuration
+---------------------------------------
+
+By default you don't have to configure anything to
+make Kirby work. For more fine-grained configuration
+of the system, please check out http://getkirby.com/docs/advanced/options
+
+*/
+
+c::set('autogit.branch',         'master');
+c::set('autogit.remote.name',    'kirbytest');
+c::set('autogit.remote.branch',  'master'); // same as autogit.branch option
+c::get('autogit.webhook.secret', 'jkergjjiopsbkljö');
+c::get('autogit.webhook.url',    'autogit');
+c::set('autogit.panel.user',     false);
+c::set('autogit.user.name',      'movingwater');
+c::set('autogit.user.email',     'info@movingwater.ch');
+c::set('autogit.language',       'en');
+c::set('autogit.translation', [
+    'site.update'  => 'Changed site options',
+    'page.create'  => 'Created page %s',
+    'page.update'  => 'Updated page %s',
+    'page.delete'  => 'Deleted page %s',
+    'page.sort'    => 'Sorted page %s',
+    'page.hide'    => 'Hid page %s',
+    'page.move'    => 'Moved page %1$s to %2$s',
+    'file.upload'  => 'Uploaded file %s',
+    'file.replace' => 'Replaced file %s',
+    'file.rename'  => 'Renamed file %s',
+    'file.update'  => 'Updated file %s',
+    'file.sort'    => 'Sorted file %s',
+    'file.delete'  => 'Deleted file %s',
+]);
